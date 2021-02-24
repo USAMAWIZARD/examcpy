37ed0644cbafde3b1e2e1f69d6b66651b3a62a43
USAMAWIZARD <usamawizard@gmail.com>
2021-02-25 00:47:41 +0530

    sfd

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set("view ejgine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(server.static(__dirname));
-questions={1:{"question":"this is a sample","a":0,"b":0,"c":0,"d":0}}
+questions={1:{"question":"this is a sample","a":0,"b":0,"c":0,"d":0},2:{"question":"this is a sample","a":0,"b":0,"c":0,"d":0}}
 
 
 io.on("connection", (socket) => {
@@ -21,10 +21,15 @@ io.on("connection", (socket) => {
 
   
   socket.on("addquestion", (data) => { 
-  
+
+    questions[parseInt(Object.keys(questions)[Object.keys(questions).length-1])+1]={"question":data,"a":0,"b":0,"c":0,"d":0}
   })
 });
 
 app.get("/", (req, res) => {
   res.render("voting.ejs",{"question":JSON.stringify(questions)});
+});
+
+app.get("/add", (req, res) => {
+  res.render("addquesions.ejs",{"question":JSON.stringify(questions)});
 });
